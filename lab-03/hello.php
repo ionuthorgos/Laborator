f55b91bf0b7873e2f405319f47fcb3fe3a67cd9a
DESKTOP-4A5IH6L\user <horgosdumitru@gmail.com>
2017-03-13 20:42:44 +0200

    lab 2

---
+++ lab-03/hello.php
@@ -0,0 +1,11 @@
+<!--exemplu de comentariu HTML -->
+Web Development Training
+<?php
+//exemplu de comentariu
+/*
+	asd
+	asda
+	
+*/
+
+?>
---
+++ lab-03/index.php
@@ -0,0 +1,19 @@
+<?php
+$a = NULL;
+$b = true;
+$c = false;
+$d = 1;
+$e = "1";
+$f = "WDT";
+
+var_dump($a);
+echo "<br />";
+var_dump($b);
+echo "<br />";
+var_dump($c);
+echo "<br />";
+var_dump($d);
+echo "<br />";
+var_dump($e);
+echo "<br />";
+var_dump($f);
